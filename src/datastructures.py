ad569aea985f405984bf7c4e004a8c3dd7215515
Matheus Monteiro <matheusferretti96@gmail.com>
2020-11-03 00:26:40 +0000

    updating

--- src/datastructures.py
+++ src/datastructures.py
@@ -17,19 +17,25 @@ class FamilyStructure:
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
-        return randint(0, 99999999)
+        return randint(1, 99999999)
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for i in range(len(self._members)): 
+            if id == self._members[i]["id"]:
+                removed_item = self._members.pop(i)
+                return removed_item
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members: 
+            if id == member["id"]:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
